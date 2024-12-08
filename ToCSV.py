8ebaf510e1428ab765d89372fbba0c82863a9a8e
unchhohang <unchhohang@gmail.com>
2024-12-08 21:33:36 +0545

    uff

--- ToCSV.py
+++ ToCSV.py
@@ -2,6 +2,7 @@
 
 from bs4 import BeautifulSoup 
 import pandas as pd 
+import numpy as np
 
 Datas = []
 
@@ -11,13 +12,13 @@ with open('Data.html') as fp:
 for trs in soup.find_all('tr'):
     Datas.append([tds.get_text() for tds in trs if tds != '\n'])
 
-date = [pd.to_datetime(child[0]).strftime('%y-%m-%d') for child in Datas]
-open = [child[1] for child in Datas]
-high = [child[2] for child in Datas]
-low = [child[3] for child in Datas]
-close = [child[4] for child in Datas]
-adjClose = [child[5] for child in Datas]
-volume = [child[6] for child in Datas]
+date = [pd.to_datetime(child[0]).strftime('%Y-%m-%d') for child in Datas]
+open = [float(child[1].replace(',','')) for child in Datas]
+high = [float(child[2].replace(',','')) for child in Datas]
+low = [float(child[3].replace(',','')) for child in Datas]
+close = [float(child[4].replace(',','')) for child in Datas]
+adjClose = [float(child[5].replace(',','')) for child in Datas]
+volume = [child[6] for child in Datas ]
 
 df = pd.DataFrame({
     "Date": date,
@@ -26,7 +27,11 @@ df = pd.DataFrame({
     "Low": low,
     "Close": close,
     "Adj Close": adjClose,
-    "Volume": volume
+    "Volume": volume 
 })
+
+df['Volume']= df['Volume'].replace('-', np.nan)
+df['Volume']= df['Volume'].ffill()
+
 
 df.to_csv('resultData.csv',index=False)
